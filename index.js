0747037772640a5e47007d98304477d5954125c0
ghoozie <67128541+ghoozie@users.noreply.github.com>
2021-02-05 13:02:59 -0500

    Revert "testing react"
    This reverts commit bbcfdeb27284317b4ff73edcea684d1a4798fff3.

--- index.js
+++ index.js
@@ -1,71 +1,62 @@
-function Intro() {
-    return <div>
-            <header id="masthead" className="site-header" data-anchor-target=".hero" data-top="background: rgba(10,10,30,1); padding: 30px 0; box-shadow: 0px 0px 20px 6px rgba(0, 0, 0, 0);" data-top-bottom="background: rgba(0,0,0,1); padding: 10px 0; box-shadow: 0px 0px 20px 6px rgba(0, 0, 0, 0.2);">
-                   <nav id="primary-navigation" className="site-navigation">
-                       <div className="container">
-                           <div className="navbar-header page-scroll">
+function intro() {
+    return <header id="masthead" class="site-header" data-anchor-target=".hero" data-top="background: rgba(10,10,30,1); padding: 30px 0; box-shadow: 0px 0px 20px 6px rgba(0, 0, 0, 0);" data-top-bottom="background: rgba(0,0,0,1); padding: 10px 0; box-shadow: 0px 0px 20px 6px rgba(0, 0, 0, 0.2);">
+                   <nav id="primary-navigation" class="site-navigation">
+                       <div class="container">
+                           <div class="navbar-header page-scroll">
 
-                               <button type="button" className="navbar-toggle collapsed" data-target="#portfolio-perfect-collapse" aria-expanded="false" >
-                                   <span className="icon-bar"></span>
-                                   <span className="icon-bar"></span>
-                                   <span className="icon-bar"></span>
+                               <button type="button" class="navbar-toggle collapsed" data-target="#portfolio-perfect-collapse" aria-expanded="false" >
+                                   <span class="icon-bar"></span>
+                                   <span class="icon-bar"></span>
+                                   <span class="icon-bar"></span>
                                </button>
-                               <a href="#"><img src="assets/img/ghoozie.png" width="160px" /></a>
+                               <a href="#"><img src="assets/img/ghoozie.png" width="160px"></a>
 
-                           </div>
-                           {/* <!-- /.navbar-header --> */}
+                           </div><!-- /.navbar-header -->
 
-                           <div className="main-menu" id="portfolio-perfect-collapse">
+                           <div class="main-menu" id="portfolio-perfect-collapse">
 
-                               <ul className="nav navbar-nav navbar-right">
+                               <ul class="nav navbar-nav navbar-right">
 
-                                   <li className="page-scroll"><a href="#hero">Home</a></li>
-                                   <li className="page-scroll"><a href="#about">About</a></li>
-                                   <li className="page-scroll"><a href="#service">Libraries</a></li>
-                                   <li className="page-scroll"><a href="#portfolio">Portfolio</a></li>
-                                   <li className="page-scroll"><a href="#contact">Contact</a></li>
+                                   <li class="page-scroll"><a href="#hero">Home</a></li>
+                                   <li class="page-scroll"><a href="#about">About</a></li>
+                                   <li class="page-scroll"><a href="#service">Libraries</a></li>
+                                   <li class="page-scroll"><a href="#portfolio">Portfolio</a></li>
+                                   <li class="page-scroll"><a href="#contact">Contact</a></li>
 
-                               </ul>
-                               {/* <!-- /.navbar-nav --> */}
+                               </ul><!-- /.navbar-nav -->
 
-                           </div>
-                           {/* <!-- /.navbar-collapse --> */}
+                           </div><!-- /.navbar-collapse -->
 
                        </div>
-                   </nav>
-                   {/* <!-- /.primary-navigation --> */}
-               </header>
-               {/*<!-- /#header -->*/}
+                   </nav><!-- /.primary-navigation -->
+               </header><!-- /#header -->
 
-               <div id="hero" className="hero">
-                   <div className="container">
-                       <div className="row">
+               <div id="hero" class="hero">
+                   <div class="container">
+                       <div class="row">
 
-                           <div className="col-md-6 ghoozie-text-bg" style={{textAlign:'center',marginTop:'30px'}}>
-                               <h1 className="main-name">Oliver Terrell</h1>
-                               <div className="page-scroll">
-                                   <p className="job-title">Software Developer/Co-Founder of Greenstar Group</p>
-                                   <a href="#contact" className="btn btn-border">Contact Me</a>
-                                   <div className="clearfix visible-xxs"></div>
-                                   <a href="https://ghoozie.tk/oliver_resume.pdf" className="btn btn-fill" target="_blank">View Resumé</a>
-                                   <div className="clearfix visible-xxs"></div>
-                                   <a href="https://www.linkedin.com/in/oliver-terrell/" className="btn btn-primary" target="_blank">LinkedIn</a>
+                           <div class="col-md-6 ghoozie-text-bg" style="text-align:center;margin-top:30px;">
+                               <h1 class="main-name">Oliver Terrell</h1>
+                               <div class="page-scroll">
+                                   <p class="job-title">Software Developer/Co-Founder of Greenstar Group</p>
+                                   <a href="#contact" class="btn btn-border">Contact Me</a>
+                                   <div class="clearfix visible-xxs"></div>
+                                   <a href="https://ghoozie.tk/oliver_resume.pdf" class="btn btn-fill" target="_blank">View Resumé</a>
+                                   <div class="clearfix visible-xxs"></div>
+                                   <a href="https://www.linkedin.com/in/oliver-terrell/" class="btn btn-primary" target="_blank">LinkedIn</a>
                                </div>
                            </div>
 
-                           <div className="col-md-6 front-pic">
-                               <img src="assets/img/oliver.jpg" alt="oliver-terrell" className="img-fluid" style={{border:'3px solid #fff8e7'}} />
+                           <div class="col-md-6 front-pic">
+                               <img src="assets/img/oliver.jpg" alt="oliver-terrell" class="img-fluid" style="border:3px solid #fff8e7;">
                            </div>
 
                        </div>
                    </div>
-               </div>
-               </div>
-               {/* <!-- /.hero --> */}
-            ;
+               </div><!-- /.hero -->;
 }
 
 ReactDOM.render(
-    <Intro />,
-    document.getElementById('Intro')
+    <intro />,
+    document.getElementById('intro')
 );
