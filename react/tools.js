a7c7661a243b1f3e8a9c5c85d39621ce5373e7fe
ghoozie <67128541+ghoozie@users.noreply.github.com>
2021-02-17 00:32:51 -0500

    moved goreact to tools
    added tool comments

--- react/tools.js
+++ react/tools.js
@@ -1,16 +1,21 @@
-function LineBreak() {
-    return(<>
-        <hr />
-    </>);
+function goReact() {
+    var loca = window.location.href;
+    if (loca.includes('localhost')) {
+        window.location.replace(loca.slice(0,23) + 'react/Rindex.html');
+    } else {
+        window.location.replace('https://ghoozie.tk/react/Rindex.html');
+    }
 }
 
 
+// I never go anywhere without my trusty mprint
 function mprint(label,log) {
     console.log(label + ":");
     console.log(log);
 }
 
 
+// Equivalent to jQuery.toggle() method
 function showHide(elem) {
     const brick = document.getElementById(elem);
     if (brick.style.display == "none") {
@@ -18,4 +23,12 @@ function showHide(elem) {
     } else {
         brick.style.display = "none";
     }
+}
+
+
+// For if I want to play with more fun section separators later
+function LineBreak() {
+    return(<>
+        <hr />
+    </>);
 }
