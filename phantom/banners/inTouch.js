28000e47fb5df9c86c5a832d48c8f32cb3d20b93
ghoozie <67128541+ghoozie@users.noreply.github.com>
2021-02-20 15:26:24 -0500

    explode banners.js

--- phantom/banners/inTouch.js
+++ phantom/banners/inTouch.js
@@ -1,1 +1,15 @@
-document.write('<li><a href="mailto:ost@alumni.cmu.edu" style="text-decoration:none;" class="fa fa-envelope">&nbsp;ost@alumni.cmu.edu</a></li><li><a class="fa fa-phone">&nbsp;(814) 360-7508</a></li><!-- <li><a href="https://github.com/ghoozie" target="_blank" class="fas fa-laptop-code" aria-hidden="true"> GitHub</a></li> --><li><a href="https://linkedin.com/in/oliver-terrell" target="_blank" class="icon brands fas fa-linkedin" aria-hidden="true">&nbsp;LinkedIn</a></li>');
+
+// Email
+document.write('<li><a href="mailto:ost@alumni.cmu.edu" style="text-decoration:none;" class="fa fa-envelope">&nbsp;ost@alumni.cmu.edu</a></li>');
+
+
+// Phone
+document.write('<li><a class="fa fa-phone">&nbsp;(814) 360-7508</a></li>');
+
+
+// GitHub
+document.write('<li><a href="https://github.com/ghoozie" target="_blank" class="icon brands fas fa-github" aria-hidden="true"> GitHub</a></li>');
+
+
+// LinkedIn
+document.write('<li><a href="https://linkedin.com/in/oliver-terrell" target="_blank" class="icon brands fas fa-linkedin" aria-hidden="true">&nbsp;LinkedIn</a></li>');
--- phantom/banners/menu.js
+++ phantom/banners/menu.js
@@ -1,3 +1,16 @@
-var linkedIn = 'window.open("https://linkedin.com/in/oliver-terrell");return false;';
 
-document.write('<li><a href="index.html">Home</a></li><li><a href="aboutme.html">About Me</a></li><li><a href="code.html">Code Samples</a></li><!-- <li><a href="projects.html">Projects</a></li> --><li><a href="#" onclick='+linkedIn+'>LinkedIn&reg;</a></li>');
+// Index/About Me
+document.write('<li><a href="index.html">Home</a></li><li><a href="aboutme.html">About Me</a></li>');
+
+
+// Code Samples
+document.write('<li><a href="code.html">Code Samples</a></li>');
+
+
+// Projects
+// document.write('<li><a href="projects.html">Projects</a></li>');
+
+
+// LinkedIn
+const linkedIn = 'window.open("https://linkedin.com/in/oliver-terrell");return false;';
+document.write('<li><a href="#" onclick='+linkedIn+'>LinkedIn&reg;</a></li>');
