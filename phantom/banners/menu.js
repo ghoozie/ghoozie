4f97e558b27735d86ff8c072701d941fcb255446
ghoozie <67128541+ghoozie@users.noreply.github.com>
2021-02-23 14:10:45 -0500

    Update menu.js

--- phantom/banners/menu.js
+++ phantom/banners/menu.js
@@ -17,14 +17,14 @@ document.write('<li><a href="#">Projects (Coming soon)</a></li></ul><hr style="p
 
 // Greenstar
 const greenStar = 'window.open("https://greenstargroup.org");return false;';
-document.write('<ul><li><a href="#" onclick='+greenStar+'>Greenstar Group&emsp;<i class="fa-sm fas fa-external-link-alt"></i></a></li>');
+document.write('<ul><li><a href="#" onclick='+greenStar+'>Greenstar Group&ensp;<i class="fa-sm fas fa-external-link-alt"></i></a></li>');
 
 
 // Uncommon Core
 const uncommonCore = 'window.open("https://uncommoncore.io");return false;';
-document.write('<li><a href="#" onclick='+uncommonCore+'>Uncommon Core&emsp;<i class="fa-sm fas fa-external-link-alt"></i></a></li>');
+document.write('<li><a href="#" onclick='+uncommonCore+'>Uncommon Core&ensp;<i class="fa-sm fas fa-external-link-alt"></i></a></li>');
 
 
 // LinkedIn
 const linkedIn = 'window.open("https://linkedin.com/in/oliver-terrell");return false;';
-document.write('<li><a href="#" onclick='+linkedIn+'>LinkedIn&reg;&emsp;<i class="fa-sm fas fa-external-link-alt"></i></a></li></ul>');
+document.write('<li><a href="#" onclick='+linkedIn+'>LinkedIn&reg;&ensp;<i class="fa-sm fas fa-external-link-alt"></i></a></li></ul>');
