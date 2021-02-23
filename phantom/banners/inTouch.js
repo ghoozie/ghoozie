c3c13dec61c6526264e72230e66b187eea19c02e
ghoozie <67128541+ghoozie@users.noreply.github.com>
2021-02-22 21:19:07 -0500

    pages coming soon

--- phantom/banners/inTouch.js
+++ phantom/banners/inTouch.js
@@ -7,12 +7,13 @@ document.write('<li><a href="mailto:ost@alumni.cmu.edu";" class="fa fa-envelope"
 // document.write('<li><span class="fa fa-phone" style="border-bottom:none;">&nbsp;(814) 360-7508</span></li>');
 
 
+// LinkedIn
+document.write('<li><a href="https://linkedin.com/in/oliver-terrell" target="_blank" class="icon brands fas fa-linkedin" aria-hidden="true" style="border-bottom:dotted 1px rgba(88, 88, 88, 0.5);">&nbsp;LinkedIn</a></li>');
+
+
 // GitHub
 document.write('<li><a href="https://github.com/ghoozie" target="_blank" class="icon brands fas fa-github" aria-hidden="true" style="border-bottom: dotted 1px rgba(88, 88, 88, 0.5);">&nbsp;GitHub</a></li>');
 
-
-// LinkedIn
-document.write('<li><a href="https://linkedin.com/in/oliver-terrell" target="_blank" class="icon brands fas fa-linkedin" aria-hidden="true" style="border-bottom:dotted 1px rgba(88, 88, 88, 0.5);">&nbsp;LinkedIn</a></li>');
 
 
 // Instagram
